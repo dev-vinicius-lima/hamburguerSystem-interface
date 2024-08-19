7fac6163a4c95b4fdc833b2a596144a1f12be22a
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-08-18 20:06:04 -0400

    feat: :sparkles: creating image upload input

--- src/containers/Admin/NewProduct/index.tsx
+++ src/containers/Admin/NewProduct/index.tsx
@@ -1,9 +1,56 @@
-import { Container } from './styles'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import { Select } from '@mui/material'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { Button } from '../../../components'
+import { Container, Input, Label, LabelUpload } from './styles'
+
+interface Inputs {
+	name: string
+	price: string
+	upload: string
+}
 
 const NewProduct = () => {
+	const [filename, setFilename] = useState('')
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>()
 	return (
 		<Container>
-			<h1>newProduct</h1>
+			<form noValidate>
+				<Label>Nome</Label>
+				<Input type="text" {...register('name')} />
+
+				<Label>Preço</Label>
+				<Input type="number" {...register('price')} />
+
+				<LabelUpload>
+					<span>
+						{filename || (
+							<>
+								<CloudUploadIcon />
+								Selecionar uma imagem
+							</>
+						)}
+					</span>
+					<input
+						type="file"
+						accept="image/png, image/jpeg"
+						{...register('upload')}
+						onChange={(e) => {
+							e.target.files && setFilename(e.target.files[0].name)
+						}}
+					/>
+				</LabelUpload>
+
+				<Select />
+				<Button id="button">Adicionar Produto</Button>
+			</form>
 		</Container>
 	)
 }
--- src/containers/Admin/NewProduct/styles.ts
+++ src/containers/Admin/NewProduct/styles.ts
@@ -1,5 +1,63 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-	/*  */
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	form {
+		width: 500px;
+		background-color: #fff;
+		box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+		border-radius: 10px;
+		padding: 30px;
+		display: flex;
+		flex-direction: column;
+		#button {
+			margin-top: 1.5rem;
+		}
+	}
+`
+
+export const Label = styled.label`
+	margin: 5px 0;
+`
+
+export const LabelUpload = styled.label`
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border: 1px dashed #525252;
+	border-radius: 10px;
+	padding: 10px;
+	width: 100%;
+	margin-bottom: 20px;
+
+	span {
+		color: #525252;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 16px;
+		gap: 0.5rem;
+	}
+
+	input {
+		opacity: 0;
+		width: 1px;
+	}
+`
+
+export const Input = styled.input`
+	min-width: 280px;
+	margin: 0px 0 20px 0;
+	height: 40px;
+	border-radius: 10px;
+	outline: none;
+	border: none;
+	padding: 0 15px;
+	background-color: #f2e5d4;
 `
