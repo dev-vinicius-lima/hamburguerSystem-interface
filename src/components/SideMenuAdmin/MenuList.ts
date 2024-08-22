902ed61fb19131d5a8b0b6cb6dd0a61c1c321cac
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-08-22 15:41:56 -0400

    feat: :sparkles: add item in MenuList

--- src/components/SideMenuAdmin/MenuList.ts
+++ src/components/SideMenuAdmin/MenuList.ts
@@ -1,8 +1,15 @@
+import HomeIcon from '@mui/icons-material/Home'
 import InventoryIcon from '@mui/icons-material/Inventory'
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
 import StoreIcon from '@mui/icons-material/Store'
 
 const MenuList = [
+	{
+		id: 0,
+		label: 'tela inicial',
+		path: '/',
+		icon: HomeIcon,
+	},
 	{
 		id: 1,
 		label: 'Pedidos',
